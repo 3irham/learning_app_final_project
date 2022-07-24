e31b2aac5972f3272324bb7e9ba45d6230b3235b
3irham <ikmuttaqin@gmail.com>
2022-07-24 19:33:04 +0700

    Part 12 12:14

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app_final_project/constants/r.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -12,8 +13,80 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main Page'),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.06),
+              blurRadius: 20,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+        child: BottomAppBar(
+          color: Colors.white,
+          child: Container(
+            height: 60,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Material(
+                      child: InkWell(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              R.assets.icHome,
+                              height: 20,
+                            ),
+                            Text('Home'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Material(
+                      child: InkWell(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              R.assets.icDiscuss,
+                              height: 20,
+                            ),
+                            Text('Diskusi Soal'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Material(
+                      child: InkWell(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              R.assets.icProfile,
+                              height: 20,
+                            ),
+                            Text('Profil'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
