dfc27bee8c7f52ebcf8f35684014dbd792273a5a
3irham <ikmuttaqin@gmail.com>
2022-07-27 11:02:20 +0700

    Part 17 13:39

--- lib/view/main/profile/profile_page.dart
+++ lib/view/main/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:learning_app_final_project/constants/r.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,11 +13,104 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          child: Text('Profile'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Akun Saya',
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+            letterSpacing: 1,
+          ),
         ),
+        centerTitle: true,
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                'Edit',
+                style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              )),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 28,
+              bottom: 60,
+              right: 15,
+              left: 15,
+            ),
+            decoration: BoxDecoration(
+              color: R.colors.primary,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Nama User',
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          )),
+                      Text('Nama Sekolah User',
+                          style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          )),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  R.assets.imgAvatar,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 7,
+                  color: Colors.black.withOpacity(0.25),
+                ),
+              ],
+            ),
+            margin: EdgeInsets.symmetric(
+              horizontal: 13,
+              vertical: 18,
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: 13,
+              vertical: 18,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Identitas Diri'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
