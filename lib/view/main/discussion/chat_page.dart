d8eac5502553ca16823d02b4cddf500c4e596eb8
3irham <ikmuttaqin@gmail.com>
2022-07-28 17:34:05 +0700

    Part 22

--- lib/view/main/discussion/chat_page.dart
+++ lib/view/main/discussion/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app_final_project/constants/r.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -13,7 +14,67 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat'),
+        title: Text('Diskusi Soal'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 2,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Nama User'),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: R.colors.primary,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          ),
+                        ),
+                        child: Text('Pesan'),
+                      ),
+                      Text('Waktu kirim'),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.camera),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.send),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
